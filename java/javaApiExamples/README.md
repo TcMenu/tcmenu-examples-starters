f998bfbcd54c70332ce257782fa427d0f0cb38bb
dave <dave@nutricherry.com>
2024-11-04 15:21:35 +0000

    Moved all examples into this module.

--- java/javaApiExamples/README.md
+++ java/javaApiExamples/README.md
@@ -1,1 +1,17 @@
-WebSocket Demo
+Various demonstrations of how to use the Java API in your applications. Kept as simple as possible so that you can quickly apply these in your solution.
+
+## Examples for when the API is acting as a device
+
+These examples cover the cases where the API is acting as a device, for example an embedded Raspberry PI.
+
+* An example showing a device that tries to connect to client connections - `DeviceWithClientConnectionExample.java`
+* This code allows you to make AES encryption keys `MakeEncryptionKeyExample.java`
+
+## Examples where the API is running remotely connecting to a device
+
+These examples cover the API running remotely and connecting to a device.
+
+* `ClientThatAcceptsForRemoteExample.java` an API "client" that accepts connections from a device, where the device provides the bootstrap and you can control the remote application.
+* `ConnectToRemoteDeviceServerExample.java` connect to a remote device on a particular socket configuration.
+* `StandaloneRs232Test.java` same as above but an RS232 example.
+
--- java/javaApiExamples/src/main/java/com/thecoderscorner/menu/examples/server/DeviceWithClientConnectionExample.java
+++ java/javaApiExamples/src/main/java/com/thecoderscorner/menu/examples/server/DeviceWithClientConnectionExample.java
@@ -1,11 +1,17 @@
 package com.thecoderscorner.menu.examples.server;
 
 import com.thecoderscorner.menu.auth.PreDefinedAuthenticator;
-import com.thecoderscorner.menu.domain.DomainFixtures;
+import com.thecoderscorner.menu.domain.util.DomainFixtures;
+import com.thecoderscorner.menu.domain.state.MenuTree;
+import com.thecoderscorner.menu.mgr.MenuInMenu;
 import com.thecoderscorner.menu.mgr.MenuManagerServer;
+import com.thecoderscorner.menu.remote.ConnectMode;
+import com.thecoderscorner.menu.remote.LocalIdentifier;
+import com.thecoderscorner.menu.remote.RemoteConnector;
 import com.thecoderscorner.menu.remote.encryption.AESEncryptionHandlerFactory;
 import com.thecoderscorner.menu.remote.mgrclient.ClientBasedConnectionManager;
 import com.thecoderscorner.menu.remote.protocol.ConfigurableProtocolConverter;
+import com.thecoderscorner.menu.remote.socket.SocketBasedConnector;
 
 import java.time.Clock;
 import java.util.UUID;
@@ -28,13 +34,14 @@ public class DeviceWithClientConnectionExample {
     // the UUID of this device
     public static final UUID MY_SERVER_UUID = UUID.fromString("A2BA2013-E17A-4551-8462-A0F6D15968AC");
     public static final String ENCRYPTED_AES_KEY = "A8UvLzdTzUCYeqir6DODRquIbch04kN1EuyocNqoJI4=";
+    private static final String MENU_IN_MENU_REMOTE = "192.168.0.222";
+    private static final int MENU_IN_MENU_PORT = 3333;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // First we create a few objects that are needed by both the remote connection logic and the menu manager.
         var protocol = new ConfigurableProtocolConverter(true);
         var clock = Clock.systemUTC();
         var executor = Executors.newScheduledThreadPool(4);
-
         var aesEncryptionFactory = new AESEncryptionHandlerFactory(ENCRYPTED_AES_KEY);
 
         // create the connection logic, in this case we are a device that connects to an "accepting" client.
@@ -43,9 +50,34 @@ public class DeviceWithClientConnectionExample {
         // create a basic menu manager, in any menu app we'd normally have one of these.
         var tree = DomainFixtures.fullEspAmplifierTestTree();
 
+        // now we create the menu manager object, it is responsible for handling remote connections and bootstrapping
+        // clients that connect.
         var menuManager = new MenuManagerServer(executor, tree, MY_SERVER_NAME, MY_SERVER_UUID, new PreDefinedAuthenticator(true), clock);
         menuManager.addConnectionManager(deviceRemote);
         menuManager.setBoardSerialProvider(() -> 1234);
+
+        // start optional menu in menu set up.
+
+        // optionally we can now add one or more menu-in-menu structures. See here for more information:
+        // https://tcmenu.github.io/documentation/arduino-libraries/tc-menu/tcmenu-iot/java-menu-in-menu/
+
+        // first we create a remote connector that will read the source menu and receive updates as it changes.
+        RemoteConnector connector = new SocketBasedConnector(
+                new LocalIdentifier(MY_SERVER_UUID, MY_SERVER_NAME),
+                executor, clock, protocol, MENU_IN_MENU_REMOTE, MENU_IN_MENU_PORT,
+                ConnectMode.FULLY_AUTHENTICATED, null
+        );
+
+        // now we create a menu in menu item that puts all the items in the "source" menu into our menu at an offset.
+        var menuInMenu = new MenuInMenu(connector, menuManager, MenuTree.ROOT,
+                MenuInMenu.ReplicationMode.REPLICATE_SILENTLY,
+                50100, 55000);
+
+        // now we start the menu in menu configuration, it puts the "source" menu structure into our tree and keeps it
+        // in sync in both directions.
+        menuInMenu.start();
+
+        // end optional menu in menu setup.
 
         // this finally spins up the example
         menuManager.start();
